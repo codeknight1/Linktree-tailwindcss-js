91743370c644356b4772077460d33bede99bb87c
Njong <njongemy@gmail.com>
2022-01-21 16:34:31 +0100

    Added hover states

--- public/sandbox.js
+++ public/sandbox.js
@@ -5,3 +5,16 @@ const author = document.querySelector('#author');
 const a1 = annotate(author,{type:'highlight',color:'#7f5af0'});
 
 a1.show();
+
+const image = document.querySelector('#image');
+
+
+image.addEventListener('mouseenter',()=>{
+    const a2 = annotate(image,{type:'circle',color:'#7f5af0'});
+    a2.show();
+
+})
+
+image.addEventListener('mouseleave',()=>{
+    a2.hide();
+})
